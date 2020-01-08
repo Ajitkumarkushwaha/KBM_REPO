e2d57755770add236b842750ef9094669e961840
Ajit Kumar <naa.ajitkumar@gmail.com>
2020-01-08 17:04:39 +0530

    print statement is added

--- Demo.java
+++ Demo.java
@@ -2,6 +2,9 @@ public class Demo{
   public static void main(String... args){
   
   System.out.println("GIT VCS Tool");
+  System.out.println("Welcome");
+   System.out.println("Hello");
+  
   }
 
 }
